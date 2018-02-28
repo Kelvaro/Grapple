4f97d68a75e15e0a50efcdfd574dfeb90af2600c
Unknown <blizzardkang@gmail.com>
2018-02-27 20:41:44 -0800

    Auto stash before merge of "Generate" and "origin/develop"

--- Grapple/Generator.m
+++ Grapple/Generator.m
@@ -17,7 +17,7 @@
 @interface Generator(){
     
     //Renderer *render;
-    NSArray *platforms;
+    NSMutableArray *platforms; //array that contains the vector2 of the position of the platforms
 }
 @end
 
@@ -25,12 +25,13 @@
 @implementation Generator
 
 
-float xpos; // the X-axis position of the platform
-float ypos; // the Y-axis position of the platform
+//float xpos; // the X-axis position of the platform
+//float ypos; // the Y-axis position of the platform
 
 
 //setup function here
 //initialize platforms array as an array of vector2s
+//platforms = [NSMutableArray arrayWithCapacity:20];
 
 
 -(void) Generate:(float)deltaTime{
@@ -40,34 +41,26 @@ float ypos; // the Y-axis position of the platform
     
     [self movePlatform:deltaTime];
     
-    //for each platform in array
+    //for each platform in array (int = 0; i < [platforms count]; i++)
         //[renderer renderCube:platforms[i]] //idk if this is how you get an item in an array in obj c
 }
 
 
 -(void) movePlatform:(float)deltaTime{
 //loop through platforms array and modify their x position
-    xpos = 0.1 * deltaTime;
-    if(xpos <= -10){
-        
-        
-        
-        
-        xpos = 10;
-        ypos = arc4random_uniform(3);
-        
-        //[render position: xpos, ypos]    }
-        
-        
-        
-        
-        
-    }
+    
+    //moveAmount = 0.1 * deltaTime
+    
+    //for each platform change its x position to be
+    //platform[i].x += moveAmount;
+    
+    
 }
 
 
 //SpawnPlatform will pick a random y value then add a new vector2 with the xposition equal to the right side of the screen and yposition equal to the random y
 //Store it in the array
+//ypos = arc4random_uniform(3);
 
 -(void)setXpos:(float)xp {
     xpos = xp;
